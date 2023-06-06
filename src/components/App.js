ee90b70e162fd86afb0bad42c775367985016a05
trivia-sandyc4136 <sandyc4136@gmail.com>
2023-06-06 22:10:57 +0530

    submitting code-Tue Jun 06 2023 22:10:57 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,16 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+let c=0;
+  function count(){
+   c+=1;
+   console.log(c);
+
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <button onClick={count}> Click me </button>
     </div>
   )
 }
